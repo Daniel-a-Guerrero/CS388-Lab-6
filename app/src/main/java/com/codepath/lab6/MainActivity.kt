3d5422f072fb8001be2fd859c551178a6131d3a2
Daniel_Guerrero <dag83@njit.edu>
2025-10-15 12:11:03 -0400

    Last touches

--- app/src/main/java/com/codepath/lab6/MainActivity.kt
+++ app/src/main/java/com/codepath/lab6/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.lab6
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.lab6.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -25,6 +28,12 @@ private const val TAG = "MainActivity/"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +42,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val parksFragment : ParksFragment = ParksFragment()
+        val campgroundFragment : CampgroundFragment = CampgroundFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_parks -> fragment = parksFragment
+                R.id.nav_camps -> fragment = campgroundFragment
+            }
+            replaceFragment(fragment)
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.nav_parks
 
         // Find RecyclerView and set up adapter
+        replaceFragment(ParksFragment())
+
 
     }
 }
